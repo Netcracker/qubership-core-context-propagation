7870d9af4827cae5328241564dc2e3eec7fcb707
Evgenii Stetsenko <i@estetsenko.com>
2025-09-15 14:14:47 +0300

    fix: call reinitializeRegistry() before test

--- framework-contexts/src/test/java/com/netcracker/cloud/framework/contexts/allowedheaders/AllowedHeadersPropertyTest.java
+++ framework-contexts/src/test/java/com/netcracker/cloud/framework/contexts/allowedheaders/AllowedHeadersPropertyTest.java
@@ -15,30 +15,24 @@ import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
 import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
-@ExtendWith(SystemStubsExtension.class)
+import java.util.Map;
+
+import static com.netcracker.cloud.framework.contexts.data.ContextDataRequest.CUSTOM_HEADER;
+
 class AllowedHeadersPropertyTest extends AbstractContextTestWithProperties {
-    public static final String HEADERS_ENV = "headers_allowed";
-    public static final String HEADERS_PROPERTY = "headers.allowed";
     private static final String CUSTOM_HEADER = "Custom-header-1";
     public static final String ALLOWED_HEADER = "allowed_header";
 
-    private static String headersPropertyBackup;
-
-    @SystemStub
-    private EnvironmentVariables environmentVariables = new EnvironmentVariables(HEADERS_ENV, CUSTOM_HEADER);
+    static Map<String, String> properties = Map.of("headers.allowed", CUSTOM_HEADER);
 
     @BeforeAll
     static void setup() {
-        headersPropertyBackup = System.getProperty(HEADERS_PROPERTY);
-        System.clearProperty(HEADERS_PROPERTY);
-        ContextPropagationTestUtils.reinitializeRegistry();
+        AbstractContextTestWithProperties.parentSetup(properties);
     }
 
     @AfterAll
     static void tearDown() {
-        if (headersPropertyBackup != null) {
-            System.setProperty("headers.allowed", headersPropertyBackup);
-        }
+        AbstractContextTestWithProperties.parentCleanup(properties);
     }
 
     @Test
