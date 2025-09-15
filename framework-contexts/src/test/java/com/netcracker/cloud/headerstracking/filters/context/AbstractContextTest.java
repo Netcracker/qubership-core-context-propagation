bb38f00e7e73c81fdb416afb73c7d61eb21c19cb
larkin <larkin.qubership@gmail.com>
2025-09-15 16:18:24 +0400

    chore: additional logs

--- framework-contexts/src/test/java/com/netcracker/cloud/headerstracking/filters/context/AbstractContextTest.java
+++ framework-contexts/src/test/java/com/netcracker/cloud/headerstracking/filters/context/AbstractContextTest.java
@@ -1,15 +1,19 @@
 package com.netcracker.cloud.headerstracking.filters.context;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import com.netcracker.cloud.context.propagation.core.RequestContextPropagation;
 import com.netcracker.cloud.framework.contexts.data.ContextDataRequest;
 
+@Slf4j
 public abstract class AbstractContextTest {
     public static final String CUSTOM_HEADER = "Custom-header-1";
 
     @BeforeEach
     void setup() {
+        System.out.println("TEST BeforeEach started");
         System.setProperty("headers.allowed", CUSTOM_HEADER);
         RequestContextPropagation.initRequestContext(new ContextDataRequest());
+        System.out.println("TEST BeforeEach finished");
     }
 }
--- framework-contexts/src/test/java/com/netcracker/cloud/headerstracking/filters/context/AllowedHeadersContextTest.java
+++ framework-contexts/src/test/java/com/netcracker/cloud/headerstracking/filters/context/AllowedHeadersContextTest.java
@@ -1,5 +1,6 @@
 package com.netcracker.cloud.headerstracking.filters.context;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -7,27 +8,34 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class AllowedHeadersContextTest extends AbstractContextTest {
 
     @Test
     void testRequestWithHeader() {
+        System.out.println("TEST testRequestWithHeader started");
         assertEquals("custom_value", AllowedHeadersContext.getHeaders().get(CUSTOM_HEADER));
+        System.out.println("TEST testRequestWithHeader finished");
     }
 
     @Test
     void testSetContext() {
+        System.out.println("TEST testSetContext started");
         Map<String, String> resultMap = new HashMap<>() {{
             put("header1", "value1");
             put("header2", "value2");
         }};
         AllowedHeadersContext.set(resultMap);
         assertEquals(resultMap, AllowedHeadersContext.getHeaders());
+        System.out.println("TEST testSetContext finished");
     }
 
     @Test
     void testClearContext() {
+        System.out.println("TEST testClearContext started");
         assertFalse(AllowedHeadersContext.getHeaders().isEmpty());
         AllowedHeadersContext.clear();
         assertTrue(AllowedHeadersContext.getHeaders().isEmpty());
+        System.out.println("TEST testClearContext finished");
     }
 }
